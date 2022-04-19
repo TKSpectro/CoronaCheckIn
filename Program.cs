8c179dcc366068e4fdf125f91057ea63bb1d8d0e
Tom Kaeppler <tomkaeppler@web.de>
2022-04-19 19:29:52 +0200

    add appsettings.Local support

--- Program.cs
+++ Program.cs
@@ -1,5 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Overwrite appsettings loading so we can have a Local one f.e. db connection strings
+builder.Host.ConfigureAppConfiguration(
+    (hostingContext, config) =>
+    {
+        config.Sources.Clear();
+
+        var env = hostingContext.HostingEnvironment;
+
+        config.SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true) //load base settings
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true) //load environment settings
+                .AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true) //load local settings
+                .AddEnvironmentVariables();
+
+        if (args != null)
+        {
+            config.AddCommandLine(args);
+        }
+    });
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Covid Check-in
+
+## Examples
+
+### Read from appsettings.json
+
+```csharp
+public class HomeController : Controller
+{
+    private readonly IConfiguration _configuration;
+
+    public HomeController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public IActionResult Index()
+    {
+        bool parsedBool = bool.Parse(_configuration["is:this:enabled"]);
+        int parsedInt = int.Parse(_configuration["what:number:is:this"]);
+        string someString = _configuration["get:this:value"];
+        return View();
+    }
+}
+```
