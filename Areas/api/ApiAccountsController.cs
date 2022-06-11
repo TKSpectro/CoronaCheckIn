54f0a75ff434adbbf3c08bfb9b014d1e71d1108b
Tom Kaeppler <tomkaeppler@web.de>
2022-06-11 14:59:41 +0200

    start api auth

--- Areas/api/ApiAccountsController.cs
+++ Areas/api/ApiAccountsController.cs
@@ -1,43 +1,40 @@
-// TODO: This needs a completely new implementation with the usage of Identity
-// using System.Collections.Immutable;
-// using CoronaCheckIn.Models;
-// using Microsoft.AspNetCore.Mvc;
-// using System.Text.Json;
-// using System.Text.Json.Nodes;
-// using CoronaCheckIn.Managers;
-//
-// namespace CoronaCheckIn.Controllers.api;
-//
-// [ApiController]
-// [Route("/api/accounts")]
-// public class AccountsController : ControllerBase
-// {
-//     private readonly ILogger<AccountsController> _logger;
-//     private readonly AccountManager _accountManager;
-//
-//     public AccountsController(ILogger<AccountsController> logger, AccountManager accountManager)
-//     {
-//         _logger = logger;
-//         _accountManager = accountManager;
-//     }
-//
-//     [HttpGet]
-//     public ActionResult<IEnumerable<User>> GetAccounts()
-//     {
-//         var accounts = _accountManager.GetAccounts();
-//         return accounts.ToArray();
-//     }
-//     
-//     [HttpGet("{id}")]
-//     public ActionResult<User> GetAccount(Guid id)
-//     {
-//         var account = _accountManager.GetAccount(id);
-//         if (account == null)
-//         {
-//             return NotFound();
-//         }
-//         
-//         return account;
-//     }
-// }
+using CoronaCheckIn.Managers;
+using CoronaCheckIn.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoronaCheckIn.Areas.api
+{
+    [ApiController]
+    [Route("/api/accounts")]
+    public class AccountsController : ControllerBase
+    {
+        private readonly ILogger<AccountsController> _logger;
+        private readonly AccountManager _accountManager;
+
+        public AccountsController(ILogger<AccountsController> logger, AccountManager accountManager)
+        {
+            _logger = logger;
+            _accountManager = accountManager;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetAccounts()
+        {
+            var accounts = _accountManager.GetAccounts();
+            return accounts.ToArray();
+        }
+    
+        [HttpGet("{id}")]
+        public ActionResult<User> GetAccount(Guid id)
+        {
+            var account = _accountManager.GetAccount(id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+        
+            return account;
+        }
+    }
+}
 
--- Areas/api/ApiIndexController.cs
+++ Areas/api/ApiIndexController.cs
@@ -1,21 +1,48 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CoronaCheckIn.Areas.api;
-
-[ApiController]
-[Route("/api")]
-public class ApiIndexController : ControllerBase
+namespace CoronaCheckIn.Areas.api
 {
-    private readonly ILogger<ApiIndexController> _logger;
-
-    public ApiIndexController(ILogger<ApiIndexController> logger)
+    [ApiController]
+    [Route("/api")]
+    public class ApiIndexController : ControllerBase
     {
-        _logger = logger;
+        private readonly ILogger<ApiIndexController> _logger;
+
+        public ApiIndexController(ILogger<ApiIndexController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("")]
+        public ActionResult<string> Index()
+        {
+            return "This is the rest api of CoronaCheckIn";
+        }
+    
+        [HttpPost("login")]
+        public ActionResult<string> Login([FromBody] LoginBody body)
+        {
+            if (body.Email.Trim() == "" || body.Password.Trim() == "")
+            {
+                throw new Exception("No email or password given.");
+            }
+            return "This is the rest api of CoronaCheckIn";
+        }
+    
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> Authed()
+        {
+        
+            return "You have access to this route";
+        }
     }
 
-    [HttpGet("")]
-    public ActionResult<string> Index()
+    public class LoginBody
     {
-        return "This is the rest api of CoronaCheckIn";
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
+
