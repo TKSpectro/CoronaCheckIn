3a72c5480495a397e5c4544152a43efcf2aa54bf
Daniel <daniel.depta@fh-erfurt.de>
2022-08-07 15:35:57 +0200

    add api function to set session as infected

--- Areas/api/ApiCasesController.cs
+++ Areas/api/ApiCasesController.cs
@@ -55,6 +55,37 @@ public class ApiCasesController : ControllerBase
     }
 
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [HttpGet("set-infected-session/{id}")]
+    public ActionResult<InfectedResponse> SetSessionAsInfected(string id)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = HttpContext.Request.Headers.Authorization.ToString().Replace("Bearer ", "");
+        var secret = _config.GetValue<string>("Jwt:Secret");
+        var key = Encoding.UTF8.GetBytes(secret);
+        var issuer = _config.GetValue<string>("Jwt:Issuer");
+        if (issuer.Trim().Length == 0) issuer = "ccn";
+        var audience = _config.GetValue<string>("Jwt:Audience");
+        if (audience.Trim().Length == 0) issuer = "ccn";
+
+        tokenHandler.ValidateToken(token, new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = true,
+            ValidIssuer = issuer,
+            ValidateAudience = true,
+            ValidAudience = audience,
+        }, out SecurityToken validatedToken);
+
+        var jwtToken = (JwtSecurityToken)validatedToken;
+        var userId = (jwtToken.Claims.First(x => x.Type == "UserId").Value);
+        var sessionId = Guid.Parse(id);
+
+        _sessionManager.SetSessionAsInfected(sessionId, userId);
+        return new InfectedResponse { Message = "your session is now marked as infected! " };
+    }
+
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [HttpGet("check-sessions")]
     public ActionResult<InfectedResponse> CheckSessions()
     {
